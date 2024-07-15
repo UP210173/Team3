3ae57cf175b234c0b667a7850343a3322432c7ad
Danna Janeth Sanchez Carreon <up210508@alumnos.upa.edu.mx>
2024-07-15 12:31:57 -0600

    feat/Login: Register page done

--- frontend/src/RouterApp.js
+++ frontend/src/RouterApp.js
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { LoginPage } from './components/Login'
+import { LoginPage } from './components/Login/pages/LoginPage'
+import { RegisterPage } from './components/Login/pages/RegisterPage'
 
 export const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='login' element={ <LoginPage /> } />
+        <Route path='registrar' element={ <RegisterPage /> } />
       </Routes>
     </BrowserRouter>
   )
