fa5a78b5c84ad3cd3334d635678f6e547e404556
Danna Janeth Sanchez Carreon <up210508@alumnos.upa.edu.mx>
2024-07-15 11:48:58 -0600

    Login Page done

--- frontend/src/components/Login/pages/LoginPage.js
+++ frontend/src/components/Login/pages/LoginPage.js
@@ -1,37 +1,75 @@
-import { Box, Button, Divider, Grid, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Divider,
+  Grid,
+  InputAdornment,
+  TextField,
+  Typography,
+} from '@mui/material';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { Link } from 'react-router-dom';
 
 export const LoginPage = () => {
+
+  const [isPasswordShowed, setisPasswordShowed] = useState(false);
+
   return (
-    <Grid 
-      container 
-      p={2} 
-      display={'flex'} 
-      justifyContent={'center'} 
-      alignItems={'center'} 
-      sx={{ backgroundColor: '#a61e4d' }}
+    <Grid
+      container
+      p={2}
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      sx={{ backgroundColor: '#6aa9e9' }}
       height={'100vh'}
     >
-      <Grid xs={12} md={10} sx={{ backgroundColor: '#fff'}} borderRadius={"5px"} p={2}>
-        <Typography component="h1" variant="h5" pb={1}> Iniciar Sesión</Typography>
+      <Typography sx={{ textTransform: 'uppercase', letterSpacing: '5px', fontWeight: '900', color: '#fff'}} component="h1" variant="h4" pb={1} mb={2}>CMS Noticias</Typography>
+      <Grid
+        width={'100%'}
+        xs={12}
+        md={4}
+        sx={{ backgroundColor: '#fff' }}
+        borderRadius={'5px'}
+        boxShadow={'initial'}
+        p={4}
+      >
+        <Typography component="h2" variant="h5" pb={1}>
+          {' '}
+          Iniciar Sesión
+        </Typography>
         <Divider />
 
-        <Box component={"form"} mt={2}>
-          <Box display={"flex"} flexDirection={"column"} gap={2} mb={5}>
-            <TextField 
+        <Box component={'form'} mt={3}>
+          <Box display={'flex'} flexDirection={'column'} gap={2}>
+            <TextField placeholder='Ingresa tu correo' fullWidth label="Correo" type="email" />
+
+            <TextField
               fullWidth
-              label="Tu Correo Electrónico" 
-              type='email' 
+              placeholder='Ingresa tu contraseña'
+              label="Tu Contraseña"
+              type={`${ isPasswordShowed ? 'text': 'password'}`}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment onClick={ () => setisPasswordShowed( !isPasswordShowed ) } sx={{ cursor: 'pointer'}} position="start">
+                  {isPasswordShowed ? (<RemoveRedEyeIcon />) : (<VisibilityOffIcon />) }
+                  </InputAdornment>
+                ),
+              }}
             />
-            <Box>
-              <TextField 
-                fullWidth
-                label="Tu Contraseña" 
-                type='password' 
-              />
-            </Box>
-            <Button>Iniciar sesión</Button>
+            <Button variant='contained' sx={{ backgroundColor: '#000'}}>Iniciar sesión</Button>
           </Box>
-        </Box>  
+        </Box>
+
+        <Box display={'flex'} justifyContent={'space-between'} alignContent={'center'} mt={3}>
+          <Typography component={'p'}>No tienes una cuenta?</Typography>
+          <Link to="/registrar" style={{ color: 'inherit', textDecoration: 'none'}}>
+              <Typography sx={{ paddingBottom:'1px', borderBottom: '1px solid currentColor'}}>Crear Una</Typography>
+          </Link>
+        </Box>
       </Grid>
     </Grid>
   );
