390d130c48973dbca8a5bc49773c9a40e614af5a
Danna Janeth Sanchez Carreon <up210508@alumnos.upa.edu.mx>
2024-07-15 11:20:33 -0600

    feat/Login: Login Layout done

--- frontend/src/components/Login/pages/LoginPage.js
+++ frontend/src/components/Login/pages/LoginPage.js
@@ -1,5 +1,36 @@
+import { Box, Button, Divider, Grid, TextField, Typography } from '@mui/material';
+
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <Grid 
+      container 
+      p={2} 
+      display={'flex'} 
+      justifyContent={'center'} 
+      alignItems={'center'} 
+      sx={{ backgroundColor: '#a61e4d' }}
+      height={'100vh'}
+    >
+      <Grid xs={12} md={10} sx={{ backgroundColor: '#fff'}} borderRadius={"5px"} p={2}>
+        <Typography component="h1" variant="h5" pb={1}> Iniciar Sesión</Typography>
+        <Divider />
+
+        <Box component={"form"} mt={2}>
+          <Box display={"flex"} flexDirection={"column"} gap={2} mb={5}>
+            <TextField 
+              fullWidth
+              label="Tu Correo Electrónico" 
+              type='email' 
+            />
+            <TextField 
+              fullWidth
+              label="Tu Contraseña" 
+              type='password' 
+            />
+            <Button>Iniciar sesión</Button>
+          </Box>
+        </Box>  
+      </Grid>
+    </Grid>
+  );
+};
