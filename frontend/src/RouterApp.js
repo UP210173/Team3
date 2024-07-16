198e86329cc600e761cda0d1f160fe10f0694938
Cristian Morales <112653722+UP210173@users.noreply.github.com>
2024-07-16 16:43:28 -0600

    commit router app

--- frontend/src/RouterApp.js
+++ frontend/src/RouterApp.js
@@ -1,11 +1,20 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { LoginPage } from './components/Login'
+import { InicioPage } from './components/Inicio'
+import { PoliticaPage } from './components/Politica'
+import { TendenciasPage } from './components/Tendencias'
+import { DeportesPage } from './components/Deportes'
 
 export const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='login' element={ <LoginPage /> } />
+
+        <Route path='inicio' element={ <InicioPage /> } />
+        <Route path='politica' element={ <PoliticaPage /> } />
+        <Route path='deportes' element={ <DeportesPage /> } />
+        <Route path='tendencias' element={ <TendenciasPage /> } />
       </Routes>
     </BrowserRouter>
   )
