953abbca1f325f0741f329f8b2fb2b0fbb2dda5f
Cristian Morales <112653722+UP210173@users.noreply.github.com>
2024-07-16 16:19:15 -0600

    update politica component

--- frontend/src/RouterApp.js
+++ frontend/src/RouterApp.js
@@ -3,6 +3,7 @@ import { LoginPage } from './components/Login'
 import { InicioPage } from './components/Inicio'
 import { PoliticaPage } from './components/Politica'
 import { InicioPage } from './components/Inicio'
+import { DeportesPage } from './components/Deportes'
 
 export const RouterApp = () => {
   return (
@@ -11,6 +12,7 @@ export const RouterApp = () => {
         <Route path='login' element={ <LoginPage /> } />
         <Route path='inicio' element={ <InicioPage /> } />
         <Route path='politica' element={ <PoliticaPage /> } />
+        <Route path='deportes' element={ <DeportesPage /> } />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/components/Politica/index.js
+++ frontend/src/components/Politica/index.js
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
 export * from "./pages/PoliticaPage"
-=======
-export * from "./pages/PoliticaPage";
->>>>>>> Stashed changes
