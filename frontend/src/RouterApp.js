9a9aa5f5a0b6c0b3e19bbda4b006d154abe9e2d7
YAIRFRIAS <156372399+YAIRFRIAS@users.noreply.github.com>
2024-07-17 11:56:55 -0600

    Update RouterApp.js

---
+++ frontend/src/RouterApp.js
@@ -0,0 +1,20 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { LoginPage } from './components/Login'
+import { InicioPage } from './components/Inicio'
+import { PoliticaPage } from './components/Politica'
+import { DeportesPage } from './components/Deportes'
+import { TendenciasPage } from './components/Tendencias'
+
+export const RouterApp = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='login' element={ <LoginPage /> } />
+        <Route path='inicio' element={ <InicioPage /> } />
+        <Route path='politica' element={ <PoliticaPage /> } />
+        <Route path='deportes' element={ <DeportesPage /> } />
+        <Route path='tendencias' element={ <TendenciasPage /> } />
+      </Routes>
+    </BrowserRouter>
+  )
+}
