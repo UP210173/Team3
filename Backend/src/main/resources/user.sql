9a4e0b94d4ba6db2c543fdec4c409dba1786c683
Up210063 <up210063@alumnos.upa.edu.mx>
2024-08-01 14:46:49 -0600

    Update user.sql

--- Backend/src/main/resources/user.sql
+++ Backend/src/main/resources/user.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE IF NOT EXISTS TODOAPP;
+CREATE DATABASE IF NOT EXISTS CMS;
 
 drop user 'admin'@'localhost';
 
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 
-GRANT ALL PRIVILEGES ON TODOAPP.* TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON CMS.* TO 'admin'@'localhost';
 
-USE TODOAPP;
+USE CMS;
