a14434d796fb614e325876b2d287be05a7fdeb9f
Cristian Morales <112653722+UP210173@users.noreply.github.com>
2024-08-01 19:27:38 -0600

    commit change p02

--- Backend/src/test/java/com/example/p02/P02ApplicationTests.java
+++ Backend/src/test/java/com/example/p02/P02ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class P03ApplicationTests {
+class P02ApplicationTests {
 
 	@Test
 	void contextLoads() {
