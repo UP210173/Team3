0e234c390d49e1ee686d4c18b80f0f34be2c5289
Danna Janeth Sanchez Carreon <up210508@alumnos.upa.edu.mx>
2024-08-02 11:40:25 -0600

    Fake news data done

---
+++ frontend/src/components/ListNotices/data/news.js
@@ -0,0 +1,37 @@
+export const noticesCreated = [
+    {
+      id: 1,
+      title: "Título 1",
+      author: "Danna Carreon",
+      body: "Lorem enim officia Lorem mollit nostrud in Lorem elit pariatur irure reprehenderit et. Labore fugiat veniam deserunt duis eu nisi elit nostrud veniam.",
+      date: new Intl.DateTimeFormat( navigator.language )
+    },
+    {
+      id: 2,
+      title: "Título 2",
+      author: "Danna Carreon",
+      body: "Lorem enim officia Lorem mollit nostrud in Lorem elit pariatur irure reprehenderit et. Labore fugiat veniam deserunt duis eu nisi elit nostrud veniam.",
+      date: new Intl.DateTimeFormat( navigator.language )
+    },
+    {
+      id: 3,
+      title: "Título 3",
+      author: "Danna Carreon",
+      body: "Lorem enim officia Lorem mollit nostrud in Lorem elit pariatur irure reprehenderit et. Labore fugiat veniam deserunt duis eu nisi elit nostrud veniam.",
+      date: new Intl.DateTimeFormat( navigator.language )
+    },
+    {
+      id: 4,
+      title: "Título 4",
+      author: "Danna Carreon",
+      body: "Lorem enim officia Lorem mollit nostrud in Lorem elit pariatur irure reprehenderit et. Labore fugiat veniam deserunt duis eu nisi elit nostrud veniam.",
+      date: new Intl.DateTimeFormat( navigator.language )
+    },
+    {
+      id: 5,
+      title: "Título 5",
+      author: "Danna Carreon",
+      body: "Lorem enim officia Lorem mollit nostrud in Lorem elit pariatur irure reprehenderit et. Labore fugiat veniam deserunt duis eu nisi elit nostrud veniam.",
+      date: new Intl.DateTimeFormat( navigator.language )
+    },
+  ]
--- frontend/src/components/ListNotices/page/ListNoticies.js
+++ frontend/src/components/ListNotices/page/ListNoticies.js
@@ -1,10 +1,13 @@
 import { Container } from "@mui/material"
 import { HeaderListNotices } from "../components/HeaderListNotices"
+import { GridListNotices } from "../components/GridListNotices"
+import { noticesCreated } from "../data/news"
 
 export const ListNoticies = () => {
   return (
     <Container maxWidth="xl" sx={{ margin: "10px auto 20px"}}>
         <HeaderListNotices />
+        <GridListNotices noticies={ noticesCreated } />
     </Container>
   )
 }
