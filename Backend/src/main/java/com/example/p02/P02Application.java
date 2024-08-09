5b35cca23c55db81c3736c0610538b591b6c65d6
up210063 <up210063@alumnos.upa.edu.mx>
2024-08-08 21:04:07 -0600

    test

--- Backend/src/main/java/com/example/p02/P02Application.java
+++ Backend/src/main/java/com/example/p02/P02Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class p02Application{
+public class P02Application{
 
 	public static void main(String[] args) {
-		SpringApplication.run(p02Application.class, args);
+		SpringApplication.run(P02Application.class, args);
 	}
 
 }
