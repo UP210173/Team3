19c776be00022a743c2f135a29437d1f63bfdd7f
up210063 <up210063@alumnos.upa.edu.mx>
2024-08-08 20:23:45 -0600

    DEV

--- Backend/db/user.sql
+++ Backend/db/user.sql
@@ -2,8 +2,8 @@ CREATE DATABASE IF NOT EXISTS P02;
 
 drop user 'p02'@'localhost';
 
-CREATE USER 'p02'@'localhost' IDENTIFIED BY 'p02';
+CREATE USER 'P02'@'localhost' IDENTIFIED BY 'P02';
 
-GRANT ALL PRIVILEGES ON p02.* TO 'p02'@'localhost';
+GRANT ALL PRIVILEGES ON P02.* TO 'P02'@'localhost';
 
 USE P02;
